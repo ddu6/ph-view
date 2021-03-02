bab21e8b8b9a969e2ee023ded27f4bee7afd4b22
ddu6 <ddu6@protonmail.com>
2021-03-03 04:21:01 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import {Main} from './components/global/main'
 document.head.innerHTML=`<meta charset='utf8'>
 <meta name='viewport' content='width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0'>
 <title>PKU Hole</title>
-<link rel="icon" href="https://cdn.jsdelivr.net/gh/pkuhelper-web/webhole/public/static/favicon/256.png" type="image/png">`
+<link rel="icon" href="https://cdn.jsdelivr.net/gh/ddu6/static@0.1.0/imgs/ph.png" type="image/png">`
 document.body.style.margin='0'
 const main=new Main()
 window.main=main
