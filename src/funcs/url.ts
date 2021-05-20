1ce23ae0ee5bf8cc90f444c1cb12ea82f12a54b3
ddu6 <ddu6@protonmail.com>
2021-05-21 00:21:21 +0800

    update

--- src/funcs/url.ts
+++ src/funcs/url.ts
@@ -1,18 +1,10 @@
 export function getVersion(){
     let version='0.1.0'
-    try{
-        const url:string=new Function('return import.meta.url')()
-        const tmp=url.match(/@\d+\.\d+\.\d+/)
+    const script=document.currentScript
+    if(script instanceof HTMLScriptElement){
+        const tmp=script.src.match(/@\d+\.\d+\.\d+/)
         if(tmp!==null){
             version=tmp[0].slice(1)
-        }
-    }catch(err){
-        const script=document.currentScript
-        if(script instanceof HTMLScriptElement){
-            const tmp=script.src.match(/@\d+\.\d+\.\d+/)
-            if(tmp!==null){
-                version=tmp[0].slice(1)
-            }
         }
     }
     return version
