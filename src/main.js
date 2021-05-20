38020de60724e286f517319978f9ef836a47ca05
ddu6 <ddu6@protonmail.com>
2021-05-20 11:32:40 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import {Main} from './components/global/main'
-document.head.innerHTML=`<meta charset='utf8'>
+document.head.innerHTML+=`<meta charset='utf8'>
 <meta name='viewport' content='width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0'>
 <title>PKU Hole</title>
 <link rel="icon" href="https://cdn.jsdelivr.net/gh/ddu6/static@0.1.0/imgs/ph.png" type="image/png">`
